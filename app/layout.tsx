c8de300135046e5ea678ce2458e501341e59d2b1
matinsekhavat <matinhyphen@gmail.com>
2024-07-26 17:34:46 +0330

    setup Font Setup and Mini Grid System for whole project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Josefin_Sans } from "next/font/google";
 import "./globals.css";
+import Header from "./_ui/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const josefin = Josefin_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={josefin.className}>
+        <div className="min-h-dvh grid grid-rows-[auto_1fr] ">
+          <Header />
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
