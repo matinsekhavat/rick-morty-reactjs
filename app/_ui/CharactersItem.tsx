099cdb780a9c92dedaebcbb17e66772380e4ae96
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 13:08:20 +0330

    create CharactersItem file with validate Ts props

--- app/_ui/CharactersItem.tsx
+++ app/_ui/CharactersItem.tsx
@@ -1,28 +1,40 @@
 import { Eye } from "lucide-react";
 import Image from "next/image";
-
-function CharactersItem() {
+import { Character } from "@/app/_ui/CharactersList";
+import Button from "./Button";
+type character = {
+  character: Character;
+};
+function CharactersItem({ character }: character) {
   return (
     <div className="flex items-center gap-4 justify-between bg-slate-700 p-4  rounded-xl">
       {/* Image */}
       <div className="flex items-center gap-4">
-        <div>
-          <Image alt="" src="/logo.png" width={50} height={50} />
+        <div className="rounded-xl overflow-hidden">
+          <Image alt="" src={character.image} width={50} height={50} />
         </div>
         {/* Statue */}
         <div>
-          <p>Rick Sanchez</p>
+          <p className="flex items-center gap-2">
+            <span>{character.gender === "Male" ? "👨🏻" : "👩🏻"}</span>
+            <span>{character.name}</span>
+          </p>
           <div className="flex items-center gap-2">
-            <span className="circle green"></span>
-            Alive-human
+            <span
+              className="circle"
+              style={{
+                backgroundColor: character.status === "Alive" ? "green" : "red",
+              }}
+            ></span>
+            <span>
+              {character.status} - {character.gender}
+            </span>
           </div>
         </div>
       </div>
 
       {/* BTN */}
-      <button>
-        <Eye color="#d11f34" />
-      </button>
+      <Button id={character.id} />
     </div>
   );
 }
