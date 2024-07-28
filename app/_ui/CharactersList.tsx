6a037d883eadea1205a55ed64ea35e486cf56e11
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 13:08:57 +0330

    implement Ts  props validation in CharactersList and loop over CharactersItem.tsx file

--- app/_ui/CharactersList.tsx
+++ app/_ui/CharactersList.tsx
@@ -1,9 +1,35 @@
 import CharactersItem from "./CharactersItem";
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
 
-function CharactersList() {
+interface CharactersListProps {
+  characters: Character[];
+}
+
+async function CharactersList({ characters }: CharactersListProps) {
   return (
-    <div>
-      <CharactersItem />
+    <div className="space-y-2">
+      {characters.map((character) => (
+        <CharactersItem key={character.id} character={character} />
+      ))}
     </div>
   );
 }
