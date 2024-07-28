705c12556e1f746f67f809090537fd8455fde33d
matinsekhavat <matinhyphen@gmail.com>
2024-07-28 13:09:37 +0330

    add logic in main Charact List Component for read data from params and Fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import CharactersList from "./_ui/CharactersList";
 
-function page() {
+export const revalidate = 0;
+
+async function Page({
+  searchParams,
+}: {
+  searchParams: { name: string; characterId: string };
+}) {
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character?name=${searchParams?.name || ""}`
+  );
+  const data = await res.json();
+
   return (
-    <div
-      className="container-fluid grid grid-cols-1 sm:grid-cols-2
-    "
-    >
-      <CharactersList />
+    <div className="container-fluid grid grid-cols-1 sm:grid-cols-2">
+      <CharactersList characters={data.results?.slice(0, 5) || []} />
+      {searchParams.characterId && <p>CARD</p>}
     </div>
   );
 }
 
-export default page;
+export default Page;
